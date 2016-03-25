beaeac9d73e80f29bc42977ded5be5b9955753cd
nik-vitaly <nik.vitaly@mail.ru>
2016-03-25 13:16:28 +0300

    add vr

--- app/src/main/java/net/nikonorov/bananashake/ActivityVR.java
+++ app/src/main/java/net/nikonorov/bananashake/ActivityVR.java
@@ -1,7 +1,22 @@
 package net.nikonorov.bananashake;
 
+import android.os.Bundle;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.badlogic.gdx.backends.android.CardBoardAndroidApplication;
 import com.badlogic.gdx.backends.android.CardBoardApplicationListener;
+import com.badlogic.gdx.backends.android.CardboardCamera;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.loader.G3dModelLoader;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.utils.UBJsonReader;
 import com.google.vrtoolkit.cardboard.Eye;
 import com.google.vrtoolkit.cardboard.HeadTransform;
 import com.google.vrtoolkit.cardboard.Viewport;
@@ -10,39 +25,104 @@ import com.google.vrtoolkit.cardboard.Viewport;
  * Created by vitaly on 25.03.16.
  */
 public class ActivityVR extends CardBoardAndroidApplication implements CardBoardApplicationListener {
-    @Override
-    public void onNewFrame(HeadTransform paramHeadTransform) {
 
-    }
-
-    @Override
-    public void onDrawEye(Eye paramEye) {
-    //
-    }
-
-    @Override
-    public void onFinishFrame(Viewport paramViewport) {
-
-    }
+    private CardboardCamera cam;
+    private ModelInstance saber;
+    private ModelBatch batch;
+    private Environment environment;
+    private static final float Z_NEAR = 0.1f;
+    private static final float Z_FAR = 1000.0f;
+    private static final float CAMERA_Z = 0;//.1f;
 
     @Override
-    public void onRendererShutdown() {
-
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(this, config);
     }
 
     @Override
     public void create() {
+        cam = new CardboardCamera();
+        cam.position.set(0f, 3f, CAMERA_Z);
+        cam.lookAt(0,3f,-5f);
+        cam.near = Z_NEAR;
+        cam.far = Z_FAR;
 
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
+//        ModelBuilder modelBuilder = new ModelBuilder();
+//        model = modelBuilder.createBox(5f, 5f, 5f,
+//                new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+//                Usage.Position | Usage.Normal);
+//        instance = new ModelInstance(model);
+//        instance.transform.translate(0, 0, -50);
+
+
+        batch = new ModelBatch();
+
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 1f, 1f, 1f, 1f));
+
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 10f, 10f, 10f, 10f));
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 100f, 100f, 100f, 100f));
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, -1f, -1f, -1f, 1f));
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 1f, 1f, 1f, 1f));
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 1f, 1f, 1f, 1f));
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 1f, 1f, 1f, 1f));
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 1f, 1f, 1f, 1f));
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 1f, 1f, 1f, 1f));
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 1f, 1f, 1f, 1f));
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 1f, 1f, 1f, 1f));
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 1f, 1f, 1f, 1f));
+
+
+        environment.add(new DirectionalLight().set(Color.WHITE, -100f, -100f, -100f));
+        environment.add(new DirectionalLight().set(Color.WHITE, -100f, -100f, 100f));
+        environment.add(new DirectionalLight().set(Color.WHITE, -100f, 100f, -100f));
+        environment.add(new DirectionalLight().set(Color.WHITE, -100f, 100f, 100f));
+        environment.add(new DirectionalLight().set(Color.WHITE, 100f, -100f, -100f));
+        environment.add(new DirectionalLight().set(Color.WHITE, 100f, -100f, 100f));
+        environment.add(new DirectionalLight().set(Color.WHITE, 100f, 100f, -100f));
+        environment.add(new DirectionalLight().set(Color.WHITE, 100f, 100f, 100f));
+
+
+        environment.add(new DirectionalLight().set(Color.WHITE, -10f, -10f, -10f));
+        environment.add(new DirectionalLight().set(Color.WHITE, -10f, -10f, 10f));
+        environment.add(new DirectionalLight().set(Color.WHITE, -10f, 10f, -10f));
+        environment.add(new DirectionalLight().set(Color.WHITE, -10f, 10f, 10f));
+        environment.add(new DirectionalLight().set(Color.WHITE, 10f, -10f, -10f));
+        environment.add(new DirectionalLight().set(Color.WHITE, 10f, -10f, 10f));
+        environment.add(new DirectionalLight().set(Color.WHITE, 10f, 10f, -10f));
+        environment.add(new DirectionalLight().set(Color.WHITE, 10f, 10f, 10f));
+
+
+        environment.add(new DirectionalLight().set(Color.RED, -1f, -1f, 0f));
+
+        environment.add(new DirectionalLight().set(Color.BLUE, 1f, 1f, 0f));
+        environment.add(new DirectionalLight().set(Color.RED, -1f, 1f, 0f));
+
+
+
+
+        UBJsonReader reader = new UBJsonReader();
+        G3dModelLoader modelLoader = new G3dModelLoader(reader);
+
+        saber = new ModelInstance(modelLoader.loadModel(Gdx.files.internal("saber/saber.g3db")));
+
+        saber.transform.scl(0.4f);
+
+        saber.transform.translate(2, 5, -3);
     }
 
     @Override
     public void resize(int width, int height) {
-
     }
 
     @Override
     public void render() {
-
     }
 
     @Override
@@ -57,6 +137,46 @@ public class ActivityVR extends CardBoardAndroidApplication implements CardBoard
 
     @Override
     public void dispose() {
+        batch.dispose();
+    }
+
+    @Override
+    public void onNewFrame(HeadTransform paramHeadTransform) {
+        saber.transform.rotate(0, 1, 0, - Gdx.graphics.getDeltaTime() * 30);
 
     }
+
+    @Override
+    public void onDrawEye(Eye eye) {
+        Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        cam.setEyeViewAdjustMatrix(new Matrix4(eye.getEyeView()));
+
+        float[] perspective = eye.getPerspective(Z_NEAR, Z_FAR);
+        cam.setEyeProjection(new Matrix4(perspective));
+        cam.update();
+
+        batch.begin(cam);
+
+        batch.render(saber, environment);
+
+        batch.end();
+    }
+
+    @Override
+    public void onFinishFrame(Viewport paramViewport) {
+
+    }
+
+    @Override
+    public void onRendererShutdown() {
+
+    }
+
+    @Override
+    public void onCardboardTrigger() {
+
+    }
+
 }
