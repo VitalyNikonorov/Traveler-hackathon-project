94eb07b892360868a061fdc25b074b757131aad1
niggor <zotov.bmstu@gmail.com>
2016-03-25 22:59:56 +0300

    20 photos and cities

--- app/src/main/java/net/nikonorov/bananashake/App.java
+++ app/src/main/java/net/nikonorov/bananashake/App.java
@@ -17,16 +17,16 @@ public class App extends Application {
 
 
         cities.add(new City(0, "Australia", "Sidney", "sidney", "saber/saber.g3db"));
-        cities.add(new City(1, "Australia", "Sidney", "sidney", "saber/saber.g3db"));
+        cities.add(new City(1, "Australia", "Canberra", "canberra", "saber/saber.g3db"));
 
         cities.add(new City(2, "New Zealand", "Wellington", "wellington", "saber/saber.g3db"));
-        cities.add(new City(3, "New Zealand", "Wellington", "wellington", "saber/saber.g3db"));
+        cities.add(new City(3, "New Zealand", "Auckland", "auckland", "saber/saber.g3db"));
 
         cities.add(new City(4, "China", "Beijing", "beijing", "saber/saber.g3db"));
-        cities.add(new City(5, "China", "Beijing", "beijing", "saber/saber.g3db"));
+        cities.add(new City(5, "China", "Shanghai", "shanghai", "saber/saber.g3db"));
 
         cities.add(new City(6, "Japan", "Tokyo", "tokyo", "saber/saber.g3db"));
-        cities.add(new City(7, "Japan", "Tokyo", "tokyo", "saber/saber.g3db"));
+        cities.add(new City(7, "Japan", "Yokohama", "yokohama", "saber/saber.g3db"));
 
         cities.add(new City(8, "USA", "Los Angeles", "la", "saber/saber.g3db"));
         cities.add(new City(9, "USA", "New York", "nyc", "saber/saber.g3db"));
@@ -35,10 +35,10 @@ public class App extends Application {
         cities.add(new City(11, "Canada", "Collingwood", "collingwood", "saber/saber.g3db"));
 
         cities.add(new City(12, "Egypt", "Cairo", "cairo", "saber/saber.g3db"));
-        cities.add(new City(13, "Egypt", "Cairo", "cairo", "saber/saber.g3db"));
+        cities.add(new City(13, "Egypt", "Hurghada", "hurghada", "saber/saber.g3db"));
 
         cities.add(new City(14, "Marocco", "Rabat", "rabat", "saber/saber.g3db"));
-        cities.add(new City(15, "Marocco", "Rabat", "rabat", "saber/saber.g3db"));
+        cities.add(new City(15, "Marocco", "Casablanca", "casablanca", "saber/saber.g3db"));
 
         cities.add(new City(16, "Russia", "Moscow", "moscow", "saber/saber.g3db"));
         cities.add(new City(17, "Russia", "Saint Petersburg", "piter", "saber/saber.g3db"));
