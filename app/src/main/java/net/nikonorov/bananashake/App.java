0d2982cce7451105899c8069885a0c363ffc0a6d
niggor <zotov.bmstu@gmail.com>
2016-03-26 12:37:38 +0300

    coordinates

--- app/src/main/java/net/nikonorov/bananashake/App.java
+++ app/src/main/java/net/nikonorov/bananashake/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
 
-        cities.add(new City(0, "Австралия", "Сидней", "sidney", "saber/saber.g3db", 47.706944, -104.1925, 1, 5));
+        cities.add(new City(0, "Австралия", "Сидней", "sidney", "saber/saber.g3db", -33.7945148, 150.3619664, 1, 5));
         cities.add(new City(1, "Австралия", "Канберра", "canberra", "saber/saber.g3db",-35.306946, 149.195,  2, 9));
 
         cities.add(new City(2, "Новая Зеландия", "Веллингтон", "wellington", "saber/saber.g3db", 37.39411, -97.423225, 3, 13));
@@ -30,7 +30,7 @@ public class App extends Application {
         cities.add(new City(6, "Япония", "Токио", "tokyo", "saber/saber.g3db", 35.76472, 140.38638, 2, 10));
         cities.add(new City(7, "Япония", "Йокогама", "yokohama", "saber/saber.g3db", 35.5075, 139.6175, 3, 14));
 
-        cities.add(new City(8, "США", "Лос-Анджелес", "la", "saber/saber.g3db", -37.40173, -72.425446,  4, 20));
+        cities.add(new City(8, "США", "Лос-Анджелес", "la", "saber/saber.g3db", 34.0207504, -118.6919282,  4, 20));
         cities.add(new City(9, "США", "Нью-Йорк", "nyc", "saber/saber.g3db", 40.777245, -73.872604, 3, 14));
 
         cities.add(new City(10, "Канада", "Брантфорд", "brantford", "saber/saber.g3db", 43.13139, -80.3425, 1, 5));
